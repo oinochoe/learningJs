33f1389127c880015855a2dd07fbea8578fb1fca
Yeognmin Kim <copstyle86@gmail.com>
2019-02-25 00:38:32 +0900

    [a] 클릭시 버블버블 ~ 거룩한 헬로우월드

--- a/main.js
+++ a/main.js
@@ -4,13 +4,20 @@ $(document).ready(function(){
     paper.install(window);                               // paper를 전역 스코프에 설치
     paper.setup(document.getElementById('mainCanvas'));  // paper.js를 캔버스에 연결, paper.js가 그림을 그릴 수 있도록 준비
 
-    var c;                                               // 바둑판식으로 여러개 그리기 원 64개
-    for(var x=25; x<400; x+=50) {
-        for(var y=25; y<400; y+=50) {
-            c = Shape.Circle(x, y, 20);
-            c.fillColor = 'rgb(255,100,20)';
-        }
-    }
+    var c = Shape.Circle(200, 200, 80);
+    c.fillColor = 'black';
+    var text = new PointText(200, 200);
+        text.justification = 'center';
+        text.fillColor = 'white';
+        text.fontSize = 20;
+        text.content = 'hello world';
+
+    var tool = new Tool();                               // 객체를 만들어 이벤트 핸들러를 연결하자.
+
+    tool.onMouseDown = function(event) {                 // onMouseDown 이벤트 핸들러 연결.
+        var c = Shape.Circle(event.point.x, event.point.y, 20);
+        c.fillColor = 'green';
+    };
 
     paper.view.draw();                                   // paper.js 가 실제로 화면에 그림을 그리라는 명령
 });
