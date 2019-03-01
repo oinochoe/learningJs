adcf219c124e34aa81cbe6195d491b6355f8d02f
Yeognmin Kim <copstyle86@gmail.com>
2019-03-01 22:17:35 +0900

    [babel] es6를 es5로 변환, gulp-babel 사용

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
-// 걸프 의존성을 사용합니다
+const babel = require('gulp-babel');
 
 gulp.task('default', async function() {
-    // 걸프 작업을 여기 씁니다.
+    // 노드 소스
+    gulp.src("es6/**/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("dist"));
+    // 브라우저 소스
+    gulp.src("public/es6/**/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("public/dist"));
 });
